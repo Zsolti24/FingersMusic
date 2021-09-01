d2c8b2d2d749f06526bff8601b32df227788691f
Szabolcs Kökény <kokeny.szabolcs04@gmail.com>
2021-09-01 16:59:49 +0200

    Módosítottunk

--- Nyilas game 1/Form1.cs
+++ Nyilas game 1/Form1.cs
@@ -25,7 +25,7 @@ namespace Nyilas_game_1
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("1. Zene");
+            MessageBox.Show("KITÖRÖLTEM");
         }
     }
 }
